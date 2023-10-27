c8f91830b71d624b4048196e6df7855e8e31ebc3
Kinara <kinarasafina@gmail.com>
2023-10-27 10:10:53 +0200

    initial commit

---
+++ src/main/java/nl/fontys/sioux/siouxbackend/SiouxBackendApplication.java
@@ -0,0 +1,13 @@
+package nl.fontys.sioux.siouxbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiouxBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiouxBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/fontys/sioux/siouxbackend/SiouxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.fontys.sioux.siouxbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiouxBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
