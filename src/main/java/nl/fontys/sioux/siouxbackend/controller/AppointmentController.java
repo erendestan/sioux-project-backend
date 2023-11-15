45dc6e09442a225864857c5743f30599b24aa0a7
Vlad Bucur <i516435@fhict.nl>
2023-11-15 11:07:05 +0100

    commented filterEmployees API endpoint, added AppointmentController

---
+++ src/main/java/nl/fontys/sioux/siouxbackend/controller/AppointmentController.java
@@ -0,0 +1,13 @@
+package nl.fontys.sioux.siouxbackend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/appointment")
+@RequiredArgsConstructor
+@CrossOrigin
+public class AppointmentController {
+}
--- src/main/java/nl/fontys/sioux/siouxbackend/controller/EmployeeController.java
+++ src/main/java/nl/fontys/sioux/siouxbackend/controller/EmployeeController.java
@@ -55,11 +55,11 @@ public class EmployeeController {
         return ResponseEntity.ok().body(response.getEmployees());
     }
 
-    @GetMapping("/filter")
-    public ResponseEntity<List<Employee>> filterEmployees(@RequestParam(name = "params") String parameters) {
-        List<Employee> employees = filterEmployeesUseCase.filterEmployees(parameters);
-        return ResponseEntity.ok().body(employees);
-    }
+//    @GetMapping("/filter")
+//    public ResponseEntity<List<Employee>> filterEmployees(@RequestParam(name = "params") String parameters) {
+//        List<Employee> employees = filterEmployeesUseCase.filterEmployees(parameters);
+//        return ResponseEntity.ok().body(employees);
+//    }
 
     @PutMapping("{id}")
     public ResponseEntity<Void> updateEmployee(@PathVariable("id") long id, @RequestBody @Valid UpdateEmployeeRequest request){
