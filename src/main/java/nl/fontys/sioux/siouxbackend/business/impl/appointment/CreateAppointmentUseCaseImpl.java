40eda473572ac18d9c867b2c617ae4e46e40bc60
Kinara <kinarasafina@gmail.com>
2023-11-20 16:18:16 +0100

    added imports

--- src/main/java/nl/fontys/sioux/siouxbackend/business/impl/appointment/CreateAppointmentUseCaseImpl.java
+++ src/main/java/nl/fontys/sioux/siouxbackend/business/impl/appointment/CreateAppointmentUseCaseImpl.java
@@ -16,6 +16,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
