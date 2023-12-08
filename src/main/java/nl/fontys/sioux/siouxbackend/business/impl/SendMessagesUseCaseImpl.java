e8698901893c520298968d85af26a349a0c60577
Kinara <kinarasafina@gmail.com>
2023-12-08 11:20:42 +0100

    send employees email if client has arrived

--- src/main/java/nl/fontys/sioux/siouxbackend/business/impl/SendMessagesUseCaseImpl.java
+++ src/main/java/nl/fontys/sioux/siouxbackend/business/impl/SendMessagesUseCaseImpl.java
@@ -2,12 +2,14 @@ package nl.fontys.sioux.siouxbackend.business.impl;
 
 import lombok.AllArgsConstructor;
 import nl.fontys.sioux.siouxbackend.business.interf.SendMessagesUseCase;
+import nl.fontys.sioux.siouxbackend.business.interf.appointment.SendAppointmentEmailUseCase;
 import nl.fontys.sioux.siouxbackend.domain.request.parking.ParkingAccessRequest;
 import nl.fontys.sioux.siouxbackend.repository.AppointmentRepository;
 import nl.fontys.sioux.siouxbackend.repository.entity.AppointmentEntity;
 import nl.fontys.sioux.siouxbackend.repository.entity.EmployeeEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
@@ -16,6 +18,7 @@ import java.util.Optional;
 @AllArgsConstructor
 public class SendMessagesUseCaseImpl implements SendMessagesUseCase {
     private final AppointmentRepository appointmentRepository;
+    private final SendAppointmentEmailUseCase sendAppointmentEmailUseCase;
     @Override
     public String handleUpcomingAppointmentNotifications(ParkingAccessRequest request) {
         // Get current time
@@ -41,8 +44,43 @@ public class SendMessagesUseCaseImpl implements SendMessagesUseCase {
 
         // TODO OPTIONAL: Send message (or email) to employees in appointment that client has arrived
         //                (and maybe at which parking lot)
-        List<EmployeeEntity> employees = appointment.get().getEmployees();
+
+        sendEmployeesEmail(appointment.get(), request);
 
         return "Appointment found but parking not full.";
+    }
+
+    private List<String> getEmployeeEmails(List<EmployeeEntity> employeeEntities){
+        List<String> emails = new ArrayList<>();
+
+        for(EmployeeEntity emp: employeeEntities){
+            emails.add(emp.getEmail());
+        }
+
+        return emails;
+    }
+
+    private void sendEmployeesEmail(AppointmentEntity appointment, ParkingAccessRequest request){
+        List<String> emails = getEmployeeEmails(appointment.getEmployees());
+        String subject = "Your client has arrived";
+
+        String htmlTemplate = """
+                <html>
+                    <body>
+                        <h1>Your client has arrived</h1>
+                        <p>Dear Employee, your client, %s, has arrived in parking lot %s</p>
+                                
+                        <h3>Appointment Details</h3>
+                        <p>Client: %s</p>
+                        <p>Start Time: %s</p>
+                        <p>End Time: %s</p>
+                        <p>Location: %s</p>
+                        <p>Description: %s</p>
+                    </body>
+                </html>
+                """;
+
+        String emailBody = String.format(htmlTemplate, appointment.getClientName(), request.isParkingTaken()? "B" : "A",appointment.getClientName(), appointment.getStartTime().toString(), appointment.getEndTime().toString(), appointment.getLocation(), appointment.getDescription());
+        sendAppointmentEmailUseCase.sendAppointmentConfirmation(emails, subject, emailBody);
     }
 }
