c9e8060241f36f7b62d4f5a36dac3cdb08ec1d89
Vlad Bucur <i516435@fhict.nl>
2023-12-12 16:47:19 +0100

    added random generating passwords, improved CSV using CsvToBeanBuilder

--- src/main/java/nl/fontys/sioux/siouxbackend/domain/DTO/EmployeeCsvDTO.java
+++ src/main/java/nl/fontys/sioux/siouxbackend/domain/DTO/EmployeeCsvDTO.java
@@ -1,33 +1,34 @@
-package nl.fontys.sioux.siouxbackend.domain.request.employee;
+package nl.fontys.sioux.siouxbackend.domain.DTO;
 
+import com.opencsv.bean.CsvBindByName;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import nl.fontys.sioux.siouxbackend.domain.Position;
 import org.hibernate.validator.constraints.Length;
 
 @Data
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class CreateEmployeeRequest {
+public class EmployeeCsvDTO {
+    @CsvBindByName(column = "first_name")
     @NotBlank
     @Length(min = 2, max = 50)
     private String firstName;
 
+    @CsvBindByName(column = "last_name")
     @NotBlank
     @Length(min = 2, max = 50)
     private String lastName;
 
+    @CsvBindByName(column = "email")
     @NotBlank
     @Email
     @Length(min = 2, max = 50)
     private String email;
 
+    @CsvBindByName(column = "position")
     @NotNull
     private Position position;
 }
