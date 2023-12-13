0f83c331ec28d6ea2a20a08bf86f615e776c33a3
Eren Destan <e.destan@student.fontys.nl>
2023-12-13 14:00:12 +0100

    SMS sender is implemented and fully working

--- src/main/java/nl/fontys/sioux/siouxbackend/business/impl/SendMessagesUseCaseImpl.java
+++ src/main/java/nl/fontys/sioux/siouxbackend/business/impl/SendMessagesUseCaseImpl.java
@@ -8,6 +8,9 @@ import nl.fontys.sioux.siouxbackend.repository.AppointmentRepository;
 import nl.fontys.sioux.siouxbackend.repository.entity.AppointmentEntity;
 import nl.fontys.sioux.siouxbackend.repository.entity.EmployeeEntity;
 import org.springframework.stereotype.Service;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -19,6 +22,9 @@ import java.util.Optional;
 public class SendMessagesUseCaseImpl implements SendMessagesUseCase {
     private final AppointmentRepository appointmentRepository;
     private final SendAppointmentEmailUseCase sendAppointmentEmailUseCase;
+    private static final String ACCOUNT_SID = "AC13ebc0b0a78e5feb2d84484ccf289988";
+    private static final String AUTH_TOKEN = "15461a02d41ca998e192de2ddff4d53b";
+    private static final String TWILIO_PHONE_NUMBER = "+12512164793";
     @Override
     public String handleUpcomingAppointmentNotifications(ParkingAccessRequest request) {
         // Get current time
@@ -39,6 +45,20 @@ public class SendMessagesUseCaseImpl implements SendMessagesUseCase {
         if (request.isParkingTaken()) {
             // TODO: Send message to client to go to other parking lot
             String clientPhoneNumber = appointment.get().getClientPhoneNumber();
+
+            Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+
+
+            String messageBody = "Welcome to Sioux! Our parking lot is currently full. Here are directions to the main parking lot: \n https://maps.app.goo.gl/EdCSYZggnNtexCEs5";
+
+            Message message = Message.creator(
+                            new PhoneNumber(formatPhoneNumber(clientPhoneNumber)),
+                            new PhoneNumber(TWILIO_PHONE_NUMBER),
+                            messageBody)
+                    .create();
+            System.out.println("Message SID: " + message.getSid());
+
+            sendEmployeesEmail(appointment.get(), request);
             return "Sent message to client at: " + clientPhoneNumber;
         }
 
@@ -82,5 +102,21 @@ public class SendMessagesUseCaseImpl implements SendMessagesUseCase {
 
         String emailBody = String.format(htmlTemplate, appointment.getClientName(), request.isParkingTaken()? "B" : "A",appointment.getClientName(), appointment.getStartTime().toString(), appointment.getEndTime().toString(), appointment.getLocation(), appointment.getDescription());
         sendAppointmentEmailUseCase.sendAppointmentConfirmation(emails, subject, emailBody);
+    }
+
+    public String formatPhoneNumber(String phoneNumber) {
+        // Check if the phone number is not empty
+        if (phoneNumber != null && !phoneNumber.isEmpty()) {
+            // Check if the phone number starts with "0"
+            if (phoneNumber.startsWith("0")) {
+                // Remove the leading "0" and prepend "+31"
+                phoneNumber = "+31" + phoneNumber.substring(1);
+            } else {
+                // If it doesn't start with "0", assume it's already in the correct format
+                phoneNumber = "+" + phoneNumber;
+            }
+        }
+
+        return phoneNumber;
     }
 }
