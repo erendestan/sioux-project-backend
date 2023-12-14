e74809720da915e79097d3604c0f947754a88cd6
Vlad Bucur <i516435@fhict.nl>
2023-12-14 22:03:53 +0100

    added constructors to EmployeeCsvDTO

--- src/main/java/nl/fontys/sioux/siouxbackend/domain/DTO/EmployeeCsvDTO.java
+++ src/main/java/nl/fontys/sioux/siouxbackend/domain/DTO/EmployeeCsvDTO.java
@@ -4,13 +4,17 @@ import com.opencsv.bean.CsvBindByName;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import nl.fontys.sioux.siouxbackend.domain.Position;
 import org.hibernate.validator.constraints.Length;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmployeeCsvDTO {
     @CsvBindByName(column = "first_name")
     @NotBlank
